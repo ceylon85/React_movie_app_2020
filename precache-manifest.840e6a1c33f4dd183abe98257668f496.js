09f0906fa2abcca172a73bf98c73ef2d99cadb26
ceylon85 <canleez123@gmail.com>
2020-02-10 17:28:05 +0900

    Updates

---
+++ precache-manifest.840e6a1c33f4dd183abe98257668f496.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a365af96e80190a70ede4daccbd10b4e",
+    "url": "/React_movie_app_2020/index.html"
+  },
+  {
+    "revision": "0ef26174834d0db64ea6",
+    "url": "/React_movie_app_2020/static/css/main.a040ce8d.chunk.css"
+  },
+  {
+    "revision": "4e09b8d67742e4a83064",
+    "url": "/React_movie_app_2020/static/js/2.1ac01af5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React_movie_app_2020/static/js/2.1ac01af5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ef26174834d0db64ea6",
+    "url": "/React_movie_app_2020/static/js/main.9c572fc9.chunk.js"
+  },
+  {
+    "revision": "16b0f02a75113d753ab6",
+    "url": "/React_movie_app_2020/static/js/runtime-main.01ce304c.js"
+  }
+]);
